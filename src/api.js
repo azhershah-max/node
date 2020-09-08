c487f83c451c54944ecba75a45da0606621ffec9
azhershah-max <70626246+azhershah-max@users.noreply.github.com>
2020-09-08 08:18:03 +0500

    Update api.js

--- src/api.js
+++ src/api.js
@@ -2,9 +2,12 @@ const express = require("express");
 const serverless = require("serverless-http");
 const ytdl = require('ytdl-core');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    exposedHeaders: ['Content-Disposition', 'Content-Length'],
+}));
 const router = express.Router();
 
 function formatBytes(bytes, decimals = 2) {
@@ -33,31 +36,48 @@ function sec2min(time) {
 }
 
 router.get("/", async (req, res) => {
-  res.header("Content-Type", 'application/json');
-  res.setHeader('Access-Control-Allow-Origin', '*');
 
-  try {
-    let id = req.query.id;
-    let info = await ytdl.getInfo(id);
-    //let a = info.formats;
-    let videoaudio = ytdl.filterFormats(info.formats, 'audioandvideo');
-    let audio = ytdl.filterFormats(info.formats, 'audioonly');
-    let video = ytdl.filterFormats(info.formats, 'videoonly');
-    let otherFormats = info.player_response.streamingData.adaptiveFormats;
-    let a = {};
-    a.videoId = info.videoDetails.videoId;
-    a.title = info.videoDetails.title;
+    let URL = req.query.URL;
+    let type = req.query.type;
+	let format = req.query.format ? req.query.format : 'mp4';
 
-    let posters = info.videoDetails.thumbnail.thumbnails;
-    a.poster = posters[posters.length - 1].url;
-    a.videoaudio = videoaudio;
-    a.video = video;
-    a.audio = audio;
-    a.otherFormats = otherFormats;
-    res.send(JSON.stringify(a, null, 4));
-  } catch (error) {
-    console.log(error);
-  }
+    if( type === 'video' ) {
+        let resp = ytdl.validateURL( URL );
+        console.log("----> ", resp, typeof resp)
+        if( resp ){
+            ytdl(URL, { format: 'mp4' })
+            .on("info", info => {
+                res.header('Content-Disposition', `attachment; filename="${info.videoDetails.title}.${format}"`);
+            })
+            .on("response", response => {
+                // If you want to set size of file in header
+                res.setHeader("content-length", response.headers["content-length"]);
+            }).on("error", error => {
+                return res.status(500).json( {"Error": "Couldn't find video"} )
+            }).pipe(res) 
+        
+        } else {
+            res.send(  {"Error": "Video not found"}  )
+        }
+    } else {
+		let resp = ytdl.validateURL( URL );
+        console.log("----> ", resp, typeof resp)
+        if( resp ){
+            ytdl(URL, { format: 'mp4' })
+            .on("info", info => {
+                res.header('Content-Disposition', `attachment; filename="${info.videoDetails.title}.${format}"`);
+            })
+            .on("response", response => {
+                // If you want to set size of file in header
+                res.setHeader("content-length", response.headers["content-length"]);
+            }).on("error", error => {
+                return res.status(500).json( {"Error": "Couldn't find video"} )
+            }).pipe(res) 
+        
+        } else {
+            res.send(  {"Error": "Video not found"}  )
+        }
+    }
 });
 
 app.use(`/.netlify/functions/api`, router);
